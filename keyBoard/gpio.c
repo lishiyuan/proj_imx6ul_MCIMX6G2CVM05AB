759e9bc1ff4487ecf7e53f62b4458ef7fb005a5c
Ace Shiyuan <aceshiyuanli@gmail.com>
2020-11-19 16:59:53 +0800

    update comment

--- keyBoard/gpio.c
+++ keyBoard/gpio.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 /*
- * 功能：创建KEY对应GPIO设备驱动文件
+ * 功能：根据GPIO序号创建对应的设备驱动文件
  */
 void GPIO_CreatNode(int gpioNum)
 {
@@ -40,10 +40,10 @@ void GPIO_CreatNode(int gpioNum)
 
 /*
  * 功能  ：设置GPIO引脚为输出模式
- * 参数  ：gpioNum：
- *         mode：in/on输出模式或输出模式
- * 返回值：VOS_OK :设置成功
- *         其他   :设置失败
+ * 参数  ：gpioNum   ：GPIO序号
+ *         direction ：in/on输出模式或输出模式
+ * 返回值：VOS_OK    : 设置成功
+ *         其他      : 设置失败
  */
 int GPIO_SetDirection(int gpioNum, char *direction)
 {
@@ -74,9 +74,9 @@ int GPIO_SetDirection(int gpioNum, char *direction)
 }
 
 /*
- * 功能  ：设置gpio输出电平
- * 参数  ：gpioNum: GPIO序号
- *         level  : 0、1高低电平
+ * 功能  ：设置GPIO输出电平
+ * 参数  ：gpioNum : GPIO序号
+ *         level   : 1/0 高/低电平
  */
 void GPIO_SetLevel(int gpioNum, int level)
 {
@@ -96,13 +96,11 @@ void GPIO_SetLevel(int gpioNum, int level)
     snprintf(cmd, sizeof(cmd) -1, "echo %d > /sys/class/gpio/gpio%d/value", level, gpioNum); /* 避免内存泄露 */
     system(cmd);
     memset(cmd, 0, sizeof(cmd));
-
-    //printf("The gpio node create success!\r\n");
 }
 
 /*
- * 功能  ：获取gpio电平
- * 返回值：gpio电平
+ * 功能  ：获取GPIO电平
+ * 返回值：GPIO电平
  */
 int GPIO_GetLevel(int gpioNum)
 {
